958988dc9f9d18b678d95b415b9e6ed5fa737da8
gamakad <louisjul.jul@gmail.com>
2022-09-08 01:57:52 +0200

    backend helmet/rate-limit

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const mongoose = require("mongoose");
 const path = require("path");
 
@@ -19,6 +21,30 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+
+// Helmet pour la gestion des Headers
+app.use(helmet({
+  contentSecurityPolicy: {
+      directives: {
+          imgSrc: ["'self'"],
+      },
+  },
+  crossOriginEmbedderPolicy: false,
+  crossOriginResourcePolicy: false,
+}
+))
+
+// express-rate-limit pour empecher les attaques de Brute Force
+const limiter = rateLimit({
+windowMs: 15 * 60 * 1000, // 15 minutes
+max: 100, // Limiter chaque IP à 100 requetes par `window` (ici, pour 15 minutes)
+standardHeaders: true, // Retourner les infos rate limit dans le headers `RateLimit-*`
+legacyHeaders: false, // Desactiver le headers `X-RateLimit-*`
+})
+
+app.use(limiter)
+
+// Servir le dossier images
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use("/api/auth", userRoutes);
